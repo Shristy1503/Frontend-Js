7d8ed3d6a540eb43e117c064fbf1812206ead23a
Shristy1503 <shriimishra15@gmail.com>
2025-03-01 22:46:08 +0530

    [Shristy] Add.Power of two

---
+++ While-loop/powerOfTwo.js
@@ -0,0 +1,17 @@
+const n = parseInt(process.argv[2]);
+
+if (isNaN(n) || n < 0) {
+    console.log("Please provide a valid non-negative integer as input.");
+    process.exit(1); 
+}
+
+let i = 0;
+let power = 1; 
+
+console.log(`Powers of 2 up to 2^${n} or 256:`);
+
+while (i <= n && power <= 256) {
+    console.log(`2^${i} = ${power}`);
+    power *= 2; 
+    i++;
+}
