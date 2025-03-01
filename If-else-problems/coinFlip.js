f8b02afaf8ca8f7522e94e7b3065f02670ce0ec6
Shristy1503 <shriimishra15@gmail.com>
2025-03-01 22:16:12 +0530

    [Shristy] Add.flip the coin

---
+++ If-else-problems/coinFlip.js
@@ -0,0 +1,8 @@
+
+// generate a random number
+const randomNumber = Math.random();
+
+console.log("Generated Random Number:", randomNumber);
+const result = randomNumber < 0.5 ? "Heads" : "Tails";
+
+console.log(result);
